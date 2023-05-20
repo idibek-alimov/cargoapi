a9c8b5b824da7dd5a0a7f6c4dbcebfdc7616c29b
alimov <alikcye.2001@gmail.com>
2023-05-20 23:26:26 +0300

    dsfsd

--- cargo/models.py
+++ cargo/models.py
@@ -18,8 +18,8 @@ class CalculatorData(models.Model):
     address = models.CharField(max_length=50, blank=True)
     insurance = models.BooleanField(default=False)
     comment = models.TextField(blank=True)
-    deliveryPrice = models.IntegerField(default=0)
-    deliveryTime = models.IntegerField(default=0)
+    deliveryPrice = models.IntegerField(default=0, blank=True, null=True)
+    deliveryTime = models.IntegerField(default=0, blank=True, null=True)
     checked = models.BooleanField(default=False)
 
 
@@ -28,5 +28,5 @@ class Client(models.Model):
     number = models.CharField(max_length=50, blank=True)
     comment = models.TextField()
     deliveryMethod = models.CharField(
-        max_length=50, blank=True, default="Авто")
-    checked = models.BooleanField(default=False)
+        max_length=50, blank=True, default="Авто", null=True)
+    checked = models.BooleanField(default=False, blank=True, null=True)
