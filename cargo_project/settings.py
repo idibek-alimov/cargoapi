cf79ffa113d9c25f233ad3f66c2399d8d0718153
alimov <alikcye.2001@gmail.com>
2023-05-25 15:15:07 +0300

    dsfsdfds

--- cargo_project/settings.py
+++ cargo_project/settings.py
@@ -38,6 +38,7 @@ ALLOWED_HOSTS = ["*", "cargoapi.vercel.app", ".versel.app"]
 # Application definit
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -69,6 +70,8 @@ TELEGRAM = {
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',  # new
     'django.middleware.common.CommonMiddleware',
@@ -167,3 +170,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 # DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
