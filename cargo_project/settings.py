38bd60cfb025abe8ab939395e5f075f9aaf2cad1
alimov <alikcye.2001@gmail.com>
2023-05-25 14:19:00 +0300

    sdfsdf

--- cargo_project/settings.py
+++ cargo_project/settings.py
@@ -94,7 +94,7 @@ CSRF_TRUSTED_ORIGINS = ["http://emample.com",
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -160,7 +160,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
